680d6aebddfd33956b02aa8f25cbfa623f9d1ca8
Azpers <Azpers@github.com>
2022-11-24 02:28:53 -0300

    starting to work on js animations, now gotta make portfolio text drag more user attention and start working on the main body

--- js/myapps.js
+++ js/myapps.js
@@ -1,2 +1,16 @@
-// const lengthTree = document.querySelector('.write-tree g path');
-// const path = lengthTree.getTotalLength();
+const tree = document.querySelector('.tree');
+const svgText = tree.querySelectorAll('.position');
+const svgTextPortfolio = tree.querySelector('.position-portfolio');
+
+svgTextPortfolio.addEventListener('click', () => {
+    tree.classList.remove('tree-in');
+    // void tree.offsetWidth;
+    tree.firstElementChild.classList.add('tree-out');
+    svgText[0].classList.remove('svg-name-animation-in');
+    svgText[1].classList.remove('svg-lastName-animation-in');
+    svgText[2].classList.remove('svg-portfolio-animation-in');
+    // void svgText[index].offsetWidth;
+    svgText[0].classList.add('svg-name-animation-back-in');
+    svgText[1].classList.add('svg-lastName-animation-back-in');
+    svgText[2].classList.add('svg-portfolio-animation-back-in');
+});
