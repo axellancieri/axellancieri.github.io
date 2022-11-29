815728b04f9578b24c1775c72df3e6964ca9fd0e
Azpers <Azpers@github.com>
2022-11-29 17:00:56 -0300

    starting to finish design layer on tree, yellow colored. Saving point

--- js/myapps.js
+++ js/myapps.js
@@ -2,15 +2,16 @@ const tree = document.querySelector('.tree');
 const svgText = tree.querySelectorAll('.position');
 const svgTextPortfolio = tree.querySelector('.position-portfolio');
 
-svgTextPortfolio.addEventListener('click', () => {
-    tree.classList.remove('tree-in');
-    // void tree.offsetWidth;
-    tree.classList.add('tree-out'); //firstElementChild
-    svgText[0].classList.remove('svg-name-animation-in');
-    svgText[1].classList.remove('svg-lastName-animation-in');
-    svgText[2].classList.remove('svg-portfolio-animation-in');
-    // void svgText[index].offsetWidth;
-    svgText[0].classList.add('svg-name-animation-back-in');
-    svgText[1].classList.add('svg-lastName-animation-back-in');
-    svgText[2].classList.add('svg-portfolio-animation-back-in');
-});
+// svgTextPortfolio.addEventListener('click', () => {
+//     tree.firstElementChild.classList.remove('tree-in');
+//     void tree.offsetWidth;
+//     tree.firstElementChild.classList.add('tree-out');
+//     tree.style.position = 'absolute';
+//     svgText[0].classList.remove('svg-name-animation-in');
+//     svgText[1].classList.remove('svg-lastName-animation-in');
+//     svgText[2].classList.remove('svg-portfolio-animation-in');
+//     void svgText[index].offsetWidth;
+//     svgText[0].classList.add('svg-name-animation-back-in');
+//     svgText[1].classList.add('svg-lastName-animation-back-in');
+//     svgText[2].classList.add('svg-portfolio-animation-back-in');
+// });
