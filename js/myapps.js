fd4625c13f9046cc256a08d7f1d375d2fc5681e6
Azpers <axel_lancieri@DESKTOP-FKU98KF.localdomain>
2022-12-02 00:58:44 -0300

    finished tree animation, gonna start working now on sign animation

--- js/myapps.js
+++ js/myapps.js
@@ -1,32 +1,40 @@
 const treeSvg = document.querySelector('.tree');
-const treeInSvg = document.querySelector('.tree-in');
-const treeAllButTextSvg = Array.from(treeSvg.querySelectorAll('use, g, #squirrel-left, #squirrel-right, #leafs')); 
+const treeAllButTextSvg = Array.from(treeSvg.querySelectorAll('use, g, #squirrel-left, #squirrel-right, #leafs')); // Need to target infinite animations individually so they'll stop during .tree-up animation
 const textSvg = Array.from(treeSvg.querySelectorAll('.position'));
 const hexagonSvg = treeSvg.querySelector('.hexagon');
 const soilSvg = treeSvg.querySelector('.soil');
 
-treeInSvg.addEventListener('click', () => {
-    treeSvg.classList.remove('tree-in');
-    treeAllButTextSvg.forEach((element, index) => {
-        if(index !== 9) {
-            element.classList.add('tree-out');
-        }
-    });
-    treeSvg.classList.add('tree-up');
-    textSvg.forEach((text) => {
-        text.classList.remove('animation-name-in', 'animation-lastName-in', 'animation-portfolio-in');
-        text.classList.add('animation-name-up-js');
-    });
-    soilSvg.classList.add('soil-animation-fade');
-    hexagonSvg.classList.add('hexagon-animation');
-    hexagonSvg.addEventListener('animationend', () => {
+treeSvg.addEventListener('click', () => {
+
+    if (treeSvg.classList.contains("tree-in")) {
+
+        treeSvg.classList.remove('tree-in');
+
         treeAllButTextSvg.forEach((element, index) => {
-            if(index ===     9) {
-                element.classList.remove('soil-animation-fade');
-                element.classList.add('soil-animation-back-in');
+            if(index === 9) {
+                element.classList.add('soil-animation-fade');
             } else {
-                element.classList.remove('tree-out');
+                element.classList.add('tree-out');
             }
         });
-    });
+
+        treeSvg.classList.add('tree-up');
+
+        textSvg.forEach((text) => {
+            text.classList.remove('animation-name-in', 'animation-lastName-in', 'animation-portfolio-in');
+            text.classList.add('animation-name-up-js');
+        });
+
+        hexagonSvg.classList.add('hexagon-animation');
+
+        hexagonSvg.addEventListener('animationend', () => {
+            treeAllButTextSvg.forEach((element, index) => {
+                if(index === 9) {
+                    element.classList.replace('soil-animation-fade', 'soil-animation-back-in');
+                } else {
+                    element.classList.replace('tree-out', 'tree-back-in');
+                }
+            });
+        });
+    }
 });
