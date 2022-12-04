09a76b6143811b0956e997d5096be65af06ac517
Azpers <axel_lancieri@DESKTOP-FKU98KF.localdomain>
2022-12-04 17:09:39 -0300

    nav kind of finished,gonna work now on getting banners for sections

---
+++ README.md
@@ -0,0 +1,12 @@
+Notes
+
+Use 'animation-play-state: paused;' on infinite animations when offscreen or offview.
+goign to do something like treeAllButTextSvg[3].style.animationPlayState = 'paused';
+
+I'd like to like push main content to one side and make projects come from the other and the opposite direction for about me section
+
+Gonna go for modals to display get in touch information
+
+Gotta decide about leaves con column and if I'll end up using skill icons falling instead or not.
+
+Wanna do like stones will rotate each one til 90deg or flat surface then 2 rods will get off sides and a banner will show. One for each section. idk how I'll do contact info yet 
--- js/myapps.js
+++ js/myapps.js
@@ -1,42 +1,69 @@
+/* VARIABLES */
+
 const treeSvg = document.querySelector('.tree');
-const treeAllButTextSvg = Array.from(treeSvg.querySelectorAll('use, g, #squirrel-left, #squirrel-right, #leafs')); // Need to target infinite animations individually so they'll stop during .tree-up animation
+const treeAllButTextSvg = Array.from(treeSvg.querySelectorAll('use, g, #leafs, #squirrel-right, #squirrel-left')); // Need to target infinite animations individually so they'll stop during .tree-up animation
+const treeInfiniteAnimations = Array.from(treeSvg.querySelectorAll('.tree-bushes path, .leafs, .squirrel-right, .squirrel-left')); //treeInfiniteAnimations.forEach(element => element.style.animationPlayState = 'paused')
 const textSvg = Array.from(treeSvg.querySelectorAll('.position'));
 const hexagonSvg = treeSvg.querySelector('.hexagon');
 const soilSvg = treeSvg.querySelector('.soil');
 const pillar = document.querySelector('.pillar');
+const navBar = document.querySelector('nav');
 
-treeSvg.addEventListener('click', () => {
+/* FUNCTIONS */
 
-    if (treeSvg.classList.contains("tree-in")) {
+const treeFirstAnimation = () => {
+    treeSvg.classList.remove('tree-in');
 
-        treeSvg.classList.remove('tree-in');
+    treeAllButTextSvg.forEach((element) => {
+        if (element.hasAttribute('class') && element.classList.contains('soil')) {
+            element.classList.add('soil-animation-fade');
+        } else {
+            element.classList.add('tree-out');
+        }
+    });
+    treeSecondAnimation();
+}
+const treeSecondAnimation = () => {
+    treeSvg.classList.add('tree-up');
 
-        treeAllButTextSvg.forEach((element, index) => {
-            if(index === 9) {
-                element.classList.add('soil-animation-fade');
+    textSvg.forEach((text) => {
+        text.classList.remove('animation-name-in', 'animation-lastName-in', 'animation-portfolio-in');
+        text.classList.add('animation-name-up-js');
+    });
+    treeThirdAnimation();
+}
+const treeThirdAnimation = () => {
+    navBar.classList.add('appear');
+    hexagonSvg.classList.add('hexagon-animation');
+    hexagonSvg.addEventListener('animationend', () => {
+        treeAllButTextSvg.forEach((element) => {
+            if (element.hasAttribute('class') && element.classList.contains('soil')) {
+                element.classList.replace('soil-animation-fade', 'soil-animation-back-in')
             } else {
-                element.classList.add('tree-out');
+                element.classList.replace('tree-out', 'tree-back-in');
             }
         });
+        pillar.firstElementChild.classList.add('pillar-all-animation');
+    });
+}
 
-        treeSvg.classList.add('tree-up');
+/* TREE-PILLAR-ANIMATION */
 
-        textSvg.forEach((text) => {
-            text.classList.remove('animation-name-in', 'animation-lastName-in', 'animation-portfolio-in');
-            text.classList.add('animation-name-up-js');
-        });
-
-        hexagonSvg.classList.add('hexagon-animation');
-
-        hexagonSvg.addEventListener('animationend', () => {
-            treeAllButTextSvg.forEach((element, index) => {
-                if(index === 9) {
-                    element.classList.replace('soil-animation-fade', 'soil-animation-back-in');
-                } else {
-                    element.classList.replace('tree-out', 'tree-back-in');
-                }
-            });
-            pillar.firstElementChild.classList.add('pillar-all-animation');
-        });
+treeSvg.addEventListener('click', () => {
+    
+    if (treeSvg.classList.contains("tree-in")) {
+        treeFirstAnimation();
     }
+    
+});
+
+projects.addEventListener('click', () => {
+    
+    if (treeSvg.classList.contains("tree-in")) {
+        treeFirstAnimation();
+        // add flip class
+        // add collapse
+        // add new content or give it show class 
+    }
+    
 });
