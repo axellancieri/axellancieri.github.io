c30f32cf00d2f685cf37dc4acc396bc596d3db0c
Azpers <Azpers@github.com>
2023-01-19 01:03:46 -0300

    making function to console.log objecets and ses if that is the problem. Seems to be related with about

--- js/pages/about-me.js
+++ js/pages/about-me.js
@@ -1,5 +1,17 @@
 import {navButton, navBar, navDropDownClick} from '/js/modules/navbar.js';
 
+const objectsAbout = document.querySelectorAll('object');
+
+objectsAbout.forEach( element => {
+    element.addEventListener('load', loadSvg);
+});
+
+// objectsAbout.addEventListener('load', loadSvg);
+
+function loadSvg(e) {
+    console.log('rdy');
+  };
+
 /* Navbar */
 
 navButton.addEventListener('click', navDropDownClick); 
